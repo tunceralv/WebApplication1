10761c6e75738dffd7e77aec351634964b01ae52
tuncer alevli <tuncer.alevli@hotmail.com>
2024-01-01 20:03:59 +0300

    guncelleme

---
+++ WebApplication1/Controllers/BolumController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class BolumController : Controller
+    {
+        HastaneSistemiContext db = new HastaneSistemiContext();
+        public IActionResult BolumEkle(BolumTable bolum)
+        {
+            db.BolumTables.Add(bolum);
+            db.SaveChanges();
+            return View(bolum);
+        }
+    }
+}
---
+++ WebApplication1/Controllers/DoktorController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class DoktorController : Controller
+    {
+        HastaneSistemiContext db = new HastaneSistemiContext();
+        public IActionResult DoktorEkle(DoktorTable doktor)
+        {
+            db.DoktorTables.Add(doktor);
+            db.SaveChanges();
+            return View(doktor);
+        }
+    }
+}
